81dc5be874193604b6fe137bb81000341235e1b1
zilola <zilolanazarova1994@gmail.com>
2023-11-13 20:36:23 +0500

    Update recipes_controller.rb, write index action to list all user's recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,9 @@
 class RecipesController < ApplicationController
   load_and_authorize_resource
 
-  def index; end
-
-  def show; end
-
-  def new; end
-
-  def create; end
-
-  def destroy; end
+  def index
+    @user = User.find(current_user.id)
+    @recipes = @user.recipes.order(id: :asc)
+    # @recipes = @recipes.paginate(page: params[:page], per_page: 5)
+  end
 end
