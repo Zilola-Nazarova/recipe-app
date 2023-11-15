a4d27dd1df797814d06047c93a7e32d7cd32bbfe
anshuman7negi <anshumannegi777@gmail.com>
2023-11-15 13:59:06 +0530

    fixed linter errors

--- spec/features/food/food_index_spec.rb
+++ spec/features/food/food_index_spec.rb
@@ -2,26 +2,60 @@ require 'rails_helper'
 
 RSpec.feature 'Food List', type: :feature do
   before(:each) do
+    create_and_log_in_user
+  end
+
+  scenario 'User views the Food List page' do
+    create_food_record('Apple', 'kg', 10.99)
+
+    visit foods_path
+
+    expect_page_content('Apple', 'kg', '10.99')
+
+    expect(page).to have_button('Delete')
+  end
+
+  scenario 'User deletes a food record' do
+    create_food_record('Apple', 'kg', 10.99)
+
+    visit_and_delete_food_record
+
+    expect(Food.count).to eq(0)
+  end
+
+  private
+
+  def create_and_log_in_user
     User.delete_all
     @user = User.create(name: 'Tom', email: 'tom@example.com', password: 'topsecret')
     @user.confirm
-    sleep(1)
+    log_in_user('tom@example.com', 'topsecret')
+  end
 
+  def create_food_record(name, measurement_unit, price)
+    Food.create(name: name, measurement_unit: measurement_unit, price: price, user: @user)
+  end
+
+  def visit_and_delete_food_record
+    visit foods_path
+
+    within 'table' do
+      expect(page).to have_button('Delete', count: 1)
+      click_button 'Delete'
+    end
+    sleep(2)
+  end
+
+  def log_in_user(email, password)
     visit new_user_session_path
-    fill_in 'Email', with: 'tom@example.com'
-    fill_in 'Password', with: 'topsecret'
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
     click_button 'Log in'
     sleep(1)
   end
 
-  scenario 'User views the Food List page' do
-    # Create some food records for the signed-in user
-    Food.create(name: 'Apple', measurement_unit: 'kg', price: 10.99, user: @user)
-
-    visit foods_path
-
+  def expect_page_content(name, measurement_unit, price)
     expect(page).to have_link('Add Food', href: new_food_path)
-
     expect(page).to have_selector('table')
 
     within 'table' do
@@ -30,26 +64,9 @@ RSpec.feature 'Food List', type: :feature do
       expect(page).to have_selector('thead th', text: 'Price')
       expect(page).to have_selector('thead th', text: 'Action')
 
-      expect(page).to have_selector('tbody tr td', text: 'Apple')
-      expect(page).to have_selector('tbody tr td', text: 'kg')
-      expect(page).to have_selector('tbody tr td', text: '10.99')
-
-      expect(page).to have_button('Delete')
+      expect(page).to have_selector('tbody tr td', text: name)
+      expect(page).to have_selector('tbody tr td', text: measurement_unit)
+      expect(page).to have_selector('tbody tr td', text: price)
     end
-  end
-
-  scenario 'User deletes a food record' do
-    # Create a food record for the signed-in user
-    Food.create(name: 'Apple', measurement_unit: 'kg', price: 10.99, user: @user)
-
-    visit foods_path
-
-    within 'table' do
-      expect(page).to have_button('Delete', count: 1)
-      click_button 'Delete'
-    end
-    sleep(2)
-
-    expect(Food.count).to eq(0)
   end
 end
--- spec/features/food/food_new_spec.rb
+++ spec/features/food/food_new_spec.rb
@@ -5,29 +5,16 @@ RSpec.feature 'Add Food Details', type: :feature do
     User.delete_all
     @user = User.create(name: 'Tom', email: 'tom@example.com', password: 'topsecret')
     @user.confirm
-    sleep(1)
-
-    visit new_user_session_path
-    fill_in 'Email', with: 'tom@example.com'
-    fill_in 'Password', with: 'topsecret'
-    click_button 'Log in'
-    sleep(1)
+    log_in_user('tom@example.com', 'topsecret')
   end
 
   scenario 'User adds food details' do
     visit new_food_path
 
-    fill_in 'Name', with: 'Apple'
-    fill_in 'food_measurement_unit', with: 'kg'
-    fill_in 'Price', with: 10
-    fill_in 'Quantity', with: 1
+    fill_in_food_form('Apple', 'kg', 10, 1)
     click_button 'Create Food'
-    expect(page).to have_content('Food was successfully created')
-    created_food = Food.last
-    expect(created_food.name).to eq('Apple')
-    expect(created_food.measurement_unit).to eq('kg')
-    expect(created_food.price).to eq(10)
-    expect(created_food.quantity).to eq(1)
+
+    expect_page_content_after_food_creation('Apple', 'kg', 10, 1)
   end
 
   scenario 'User submits the form with invalid data' do
@@ -36,7 +23,32 @@ RSpec.feature 'Add Food Details', type: :feature do
     click_button 'Create Food'
 
     expect(Food.count).to eq(existing_food_count)
-
     expect(page).to have_content('Add Food Details')
+  end
+
+  private
+
+  def log_in_user(email, password)
+    visit new_user_session_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Log in'
+    sleep(1)
+  end
+
+  def fill_in_food_form(name, measurement_unit, price, quantity)
+    fill_in 'Name', with: name
+    fill_in 'food_measurement_unit', with: measurement_unit
+    fill_in 'Price', with: price
+    fill_in 'Quantity', with: quantity
+  end
+
+  def expect_page_content_after_food_creation(name, measurement_unit, price, quantity)
+    expect(page).to have_content('Food was successfully created')
+    created_food = Food.last
+    expect(created_food.name).to eq(name)
+    expect(created_food.measurement_unit).to eq(measurement_unit)
+    expect(created_food.price).to eq(price)
+    expect(created_food.quantity).to eq(quantity)
   end
 end
