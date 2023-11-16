ee90d546f6f6ec267e05fbfdea6384f008aed785
zilola_asus_wsl <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-11-16 13:23:50 +0500

    Change domains

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,14 +105,14 @@ Rails.application.configure do
   # config.action_mailer.perform_deliveries = true
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'https://recipe-app-mii4.onrender.com/' }
+  config.action_mailer.default_url_options = { :host => 'recipe-app-mii4.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'mail.google.com',
+    domain:               'gmail.com',
     user_name:            'recipeappcapstone@gmail.com',
-    password:             'recipe123',
+    password:             'recipe111app',
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
