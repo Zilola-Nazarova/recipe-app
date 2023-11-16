1800abf296179e8f299ea0c092a8c6b706777ad9
zilola_asus_wsl <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-11-16 12:18:57 +0500

    Add mailcatcher

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source 'https://rubygems.org'
 
 ruby '3.2.2'
 gem 'cancancan'
-# gem 'mailcatcher'
 gem 'devise', '~> 4.9'
+gem 'mailcatcher'
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.1.2'
 gem 'rubocop', '>= 1.0', '< 2.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,12 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port: 1025
+  }
+  config.action_mailer.perform_deliveries = true
 end
