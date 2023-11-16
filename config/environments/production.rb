fa58dfdcc908dc4f2d0d2e3540f7279d9719f738
zilola_asus_wsl <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-11-16 13:09:02 +0500

    Add email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,11 +96,23 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: '127.0.0.1',
+  #   port: 1025
+  # }
+  # config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: '127.0.0.1',
-    port: 1025
-  }
-  config.action_mailer.perform_deliveries = true
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'mail.google.com',
+    user_name:            'recipeappcapstone@gmail.com',
+    password:             'recipe123',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
